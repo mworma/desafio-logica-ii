07f34f25b3d8ae20049fce864a6edcb5096c5653
mworma <marcelo@worma.com.br>
2024-09-13 13:47:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+/* 2️⃣ Calculadora de partidas Rankeadas
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+- Funções
+
+## Objetivo:
+
+Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
+depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+Se vitórias for menor do que 10 = Ferro
+Se vitórias for entre 11 e 20 = Bronze
+Se vitórias for entre 21 e 50 = Prata
+Se vitórias for entre 51 e 80 = Ouro
+Se vitórias for entre 81 e 90 = Diamante
+Se vitórias for entre 91 e 100= Lendário
+Se vitórias for maior ou igual a 101 = Imortal
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**" */
+
+let vitorias = prompt("Informe o número de vitórias: ")
+let derrotas = prompt("Informe o número de derrotas: ")
+
+rankear(vitorias, derrotas)
+console.log ("O herói tem o saldo de " + saldo + " e está no nível de " + nivel)
+
+function rankear(vit, der){
+  saldo = (vit - der);
+  nivelar(saldo);
+}
+
+function nivelar(){
+  if (saldo < 11){
+      nivel = "Ferro";
+      }
+  else if (saldo < 21){
+      nivel = "Bronze";
+    }
+  else if (saldo < 51){
+      nivel = "Prata";
+      }
+  else if (saldo < 81){
+    nivel = "Ouro";
+    }
+  else if (saldo < 91){
+    nivel = "Diamante";
+    }
+  else if (saldo < 101){
+    nivel = "Lendário";
+    }
+  else
+    {
+        nivel = "Imortal";
+    }
+  return nivel
+}
